08f591fc0f5e0f328bc3a632128ba97b9558099b
Kayla The Great <owner@MacBook-Pro.local>
2019-09-03 17:25:24 -0600

    updated routes, added a marketing folder

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('marketing.index');
+});
+
+Route::get('marketing', function(){
+    return view('marketing.index');
 });
 
 Route::middleware('auth')->group(function() {
